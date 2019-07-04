b9554fb04faeaf03e9802da6a13ef1882004d6d1
Kayra Czerniewicz <kayracz@gmail.com>
2019-07-04 16:36:33 -0400

    just incase

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'aboutme', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+     get :toggle_status
+    end
+  end
 
   root to: 'pages#home'
 end
