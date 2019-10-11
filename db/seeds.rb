c0fe43080087fbfce5c6faf4d17d64c5a0c11ea5
Kayra Czerniewicz <kayracz@gmail.com>
2019-10-11 12:09:28 -0400

    sadads

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-User.create!(
-  email: "kayracz@gmail.com",
-  password: "123456",
-  password_confirmation: "123456",
-  name: "Admin User",
-  roles: "site_admin"
-)
+# User.create!(
+#   email: "kayracz@gmail.com",
+#   password: "123456",
+#   password_confirmation: "123456",
+#   name: "Admin User",
+#   roles: "site_admin"
+# )
 
-puts "1 Admin user created"
+# puts "1 Admin user created"
 
-User.create!(
-  email: "test2@test.com",
-  password: "asdfasdf",
-  password_confirmation: "asdfasdf",
-  name: "Regular User",
-)
+# User.create!(
+#   email: "test2@test.com",
+#   password: "asdfasdf",
+#   password_confirmation: "asdfasdf",
+#   name: "Regular User",
+# )
 
-puts "1 regular user created"
+# puts "1 regular user created"
 
 
-3.times do |topic|
-  Topic.create!(
-    title: "Topic #{topic}"
-  )
-end
+# 3.times do |topic|
+#   Topic.create!(
+#     title: "Topic #{topic}"
+#   )
+# end
 
-puts "3 Topics created"
+# puts "3 Topics created"
 
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    topic_id: Topic.last.id
+    # topic_id: Topic.last.id
   )
 end
 
