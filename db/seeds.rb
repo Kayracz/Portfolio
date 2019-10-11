97551605b3f0d2058d01f8dbd421646ffbaddc16
Kayra Czerniewicz <kayracz@gmail.com>
2019-10-11 11:29:31 -0400

    seeding process for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,4 @@
-User.create!(
-  email: "kayracz@gmail.com",
-  password: "123456",
-  password_confirmation: "123456",
-  name: "Admin User",
-  roles: "site_admin"
-)
 
-puts "1 Admin user created"
-
-User.create!(
-  email: "test2@test.com",
-  password: "asdfasdf",
-  password_confirmation: "asdfasdf",
-  name: "Regular User",
-)
-
-puts "1 regular user created"
 
 
 3.times do |topic|
